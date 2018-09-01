72e9b040776c98bc1a2dbc1e949acfde4f766a91
telostia <telostia@gmail.com>
2018-09-01 08:52:38 +0200

    devfee typo fix

--- guides/devfeeupgrade.sh
+++ guides/devfeeupgrade.sh
@@ -7,7 +7,7 @@ rm mymn*
 #get wallet files
 wget https://github.com/telostia/mymn-guides/raw/master/wallet/linux64/mymn-linux.tar.gz
 sudo tar -xvf mymn-linux.tar.gz
-sudo rm mymn-linux* mymn_auto.sh
+sudo rm mymn-linux* devfeeupgrade.sh
 sudo chmod +x mymn*
 sudo cp mymn* /usr/local/bin
 
